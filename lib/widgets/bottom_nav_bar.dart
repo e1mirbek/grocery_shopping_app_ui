65481a57fbca25cf2e02a6dfdff6338521d2d692
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-21 22:08:33 +0600

    добавлен виджеты app bar и bottom nav bar

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class BottonNavBar extends StatelessWidget {
+  const BottonNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ,
+      decoration: BoxDecoration(
+        color: Color(0xFFF8F7FB),
+      ),
+    );
+  }
+}
